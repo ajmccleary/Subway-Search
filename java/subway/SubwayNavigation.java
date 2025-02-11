ca714d8dc88360654b545889205929881ac943e3
Andrew McCleary <theandrewmccleary@gmail.com>
2025-02-11 18:09:30 -0500

    added SubwayNavigation class which inherits problem

---
+++ java/subway/SubwayNavigation.java
@@ -0,0 +1,22 @@
+package subway;
+
+import java.util.ArrayList;
+
+import search.Problem;
+import search.State;
+import search.Tuple;
+
+public class SubwayNavigation extends Problem {
+
+    public SubwayNavigation (State initial) {
+        super(initial);
+
+    }
+
+    public ArrayList<Tuple> successor(State state) {
+        //need to create a method which returns list of tuples of all next states and costs
+        return null;
+    }
+
+
+}
